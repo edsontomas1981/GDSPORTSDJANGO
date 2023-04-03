3bd6de1d78067b2678b505a9eb04293daeac72db
Edson <edsontomasdev@gmail.com>
2023-04-03 09:04:34 -0300

    atu

--- GDSPORTS/settings.py
+++ GDSPORTS/settings.py
@@ -74,11 +74,15 @@ WSGI_APPLICATION = 'GDSPORTS.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+      'default': {
+          'ENGINE': 'django.db.backends.postgresql_psycopg2',
+          'NAME': 'PROJECT',
+          'HOST': '/opt/bitnami/postgresql',
+          'PORT': '5432',
+          'USER': 'USERNAME',
+          'PASSWORD': 'PASSWORD'
+      }
+  }
 
 
 # Password validation
